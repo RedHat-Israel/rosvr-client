4336070c48efb35462182a2470b7036512bacf70
shakedlokits <shaked.lokits@gmail.com>
2017-08-01 09:34:14 +0300

    set a certificated local server for ssl support

--- webpack.config.js
+++ webpack.config.js
@@ -9,9 +9,6 @@ module.exports = {
 		path: path.resolve(__dirname, 'dist')
 	},
 	devtool: '#inline-source-map',
-	devServer: {
-		contentBase: './dist'
-	},
 	plugins: [
 		new CleanWebpackPlugin(['dist']),
 		new HtmlWebpackPlugin({
